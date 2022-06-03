aa79dfce777e18028652cb8a32bd6cd69c5cc19a
henrique santana bitencourt lopes <henriquehsb787@gmail.com>
2022-06-03 09:44:23 -0300

    atualização

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+
+import express from 'express'
+import cors from 'cors'
+
+const server = express();
+server.use(cors());                                                                                                         (cors());
+
+
+
+
+server.listen(process.env.PORT,
+             () => console.log (`API online na porta ${process.env.PORT}`));
